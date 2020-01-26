fce8cb493d6c44f068a742d8893e9a7596db28c6
Jefferson Lopo <jeffersonlopo@gmail.com>
2020-01-26 18:33:39 -0300

    React CRUD

---
+++ crud/frontend/src/components/home/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from "../template/Main";
+
+export default props =>
+    <Main icon="home" title='inicio'
+          subtitle="Segundo Projeto do capitulo de React">
+        <div className="display-4">Bem-vindo!!</div>
+        <hr/>
+        <p className="mb-0">Sistema para exemplificar um CRUD feito em REACT</p>
+    </Main>
